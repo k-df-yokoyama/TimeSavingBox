03916acaa960276f9633a2b7fdef3d08b1ad7939
DESKTOP-A3PUA1E\kensuke <pxd04463@nifty.ne.jp>
2020-11-28 17:50:08 +0900

    * Save starttime and endtime.

--- app/src/main/java/com/sheepduck/android/timesavingbox/MainActivity.kt
+++ app/src/main/java/com/sheepduck/android/timesavingbox/MainActivity.kt
@@ -23,13 +23,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onSaveButtonClick(view: View) {
+        val etStartTime = findViewById<EditText>(R.id.et_start_time)
+        val startTime = etStartTime.text.toString()
+        val etEndTime = findViewById<EditText>(R.id.et_end_time)
+        val endTime = etEndTime.text.toString()
         val etMemo = findViewById<EditText>(R.id.et_memo)
         val memo = etMemo.text.toString()
 
         val db = _helper.writableDatabase
 
         //val sqlInsert = "INSERT INTO timesavingbox (_id, date, starttime, endtime, memo) VALUES (?, ?, ?, ?, ?, ?)"
-        val sqlInsert = "INSERT INTO timesavingbox (date, starttime, endtime, memo) VALUES (?, ?, ?, ?)"
+        val sqlInsert = "INSERT OR REPLACE INTO timesavingbox (_id, date, starttime, endtime, memo) VALUES (1, ?, ?, ?, ?)"
         var stmt = db.compileStatement(sqlInsert)
         //TODO("LocalDate format yyyy-MM-dd 2019-07-04 is expected") //https://codechacha.com/ja/kotlin-examples-current-date-and-time/
 
@@ -37,12 +41,12 @@ class MainActivity : AppCompatActivity() {
         val formatter = DateTimeFormatter.ISO_DATE
         val formatted = current.format(formatter)
         stmt.bindString(1, formatted)
-        stmt.bindString(2, "starttime")
-        stmt.bindString(3, "endtime")
+        stmt.bindString(2, startTime)
+        stmt.bindString(3, endTime)
         stmt.bindString(4, memo)
         stmt.executeInsert()
 
         //delete input value
-        etMemo.setText("")
+        //etMemo.setText("")
     }
 }
