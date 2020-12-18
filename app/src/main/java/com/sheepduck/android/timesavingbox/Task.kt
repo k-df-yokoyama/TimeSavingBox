5a39ce030fd999cce2266431eea01ede2a553111
DESKTOP-A3PUA1E\kensuke <pxd04463@nifty.ne.jp>
2020-12-19 02:03:17 +0900

    * Add Room Entity class.

--- app/src/main/java/com/sheepduck/android/timesavingbox/Task.kt
+++ app/src/main/java/com/sheepduck/android/timesavingbox/Task.kt
@@ -1,8 +1,11 @@
 package com.sheepduck.android.timesavingbox
 
-import android.widget.EditText
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
+@Entity
 class Task(startTime:String, endTime:String, memo:String) {
+    @PrimaryKey(autoGenerate = true) var id: Int = 0
     var startTime:String = startTime
     var endTime:String = endTime
     var memo:String = memo
