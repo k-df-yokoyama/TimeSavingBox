49dcb017e783ee7ceb7f454ed6eb20b1d88e533b
DESKTOP-A3PUA1E\kensuke <pxd04463@nifty.ne.jp>
2020-12-31 16:52:07 +0900

    * Add test to Room.

---
+++ app/src/test/java/com/sheepduck/android/timesavingbox/TaskRepositoryTest.kt
@@ -0,0 +1,22 @@
+package com.sheepduck.android.timesavingbox
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class TaskRepositoryTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun insertTask() {
+    }
+}
