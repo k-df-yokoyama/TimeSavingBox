6f1cfbb5d9a3eee85eab51b75ca00217c6f2acc2
DESKTOP-A3PUA1E\kensuke <pxd04463@nifty.ne.jp>
2020-12-31 16:52:20 +0900

    * Add test to Room.

--- app/src/test/java/com/sheepduck/android/timesavingbox/TaskRepositoryTest.kt
+++ app/src/test/java/com/sheepduck/android/timesavingbox/TaskRepositoryTest.kt
@@ -1,15 +1,27 @@
 package com.sheepduck.android.timesavingbox
 
+import android.content.Context
+import android.os.Build
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 
-import org.junit.Assert.*
+import org.assertj.core.api.Assertions.*
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import androidx.test.InstrumentationRegistry
+import org.robolectric.annotation.Config
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [Build.VERSION_CODES.P])
 class TaskRepositoryTest {
+    lateinit var context: Context
 
     @Before
     fun setUp() {
+        context = InstrumentationRegistry.getTargetContext()
     }
 
     @After
@@ -17,6 +29,21 @@ class TaskRepositoryTest {
     }
 
     @Test
-    fun insertTask() {
+    fun insertTask_finishSuccessfully() {
+        var allTask = TaskRepository.loadAllTask(context)
+        assertThat(allTask).isEmpty()
+
+        val etStartTime = "2020/12/31 00:00:00"
+        val etEndTime = "2020/12/31 01:00:00"
+        val etMemo = "memo"
+        val current = LocalDate.now()
+        val formatter = DateTimeFormatter.ISO_DATE
+        val formatted = current.format(formatter)
+
+        var task:Task = Task(formatted, etStartTime, etEndTime, etMemo)
+
+        TaskRepository.insertTask(context, task)
+        allTask = TaskRepository.loadAllTask(context)
+        assertThat(allTask).hasSize(1)
     }
 }
